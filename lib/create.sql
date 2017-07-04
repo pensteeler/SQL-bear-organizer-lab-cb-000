1656e787ce4cd426cc22c273552010cff6a5463c
john vascsinec <pensteeler@gmail.com>
2017-07-04 20:45:50 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+create TABLE bears(
+
+  id	INTEGER PRIMARY KEY,
+  name	TEXT,
+  age	INTEGER,
+  gender	TEXT,
+  color	TEXT,
+  temperament	TEXT,
+  alive	INTEGER
+
+);
---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears ( id, name, age, gender, color, temperament, alive)
+VALUES (1, "Mr. Chocolate", 10, 'M', "Black", "Mean", 1 );
+
+INSERT INTO bears ( id, name, age, gender, color, temperament, alive)
+VALUES (2, "Rowdy", 40, 'M', "Black", "Mean", 1 );
+
+INSERT INTO bears ( id, name, age, gender, color, temperament, alive)
+VALUES (3, "Tabitha", 19, 'F', "Black", "Mean", 1 );
+
+INSERT INTO bears ( id, name, age, gender, color, temperament, alive)
+VALUES (4, "Sergeant Brown", 87, 'M', "Black", "Mean", 1 );
+
+INSERT INTO bears ( id, name, age, gender, color, temperament, alive)
+VALUES (5, "Melissa", 116, 'F', "Black", "Mean", 1 );
+
+INSERT INTO bears ( id, name, age, gender, color, temperament, alive)
+VALUES (6, "Grinch", 50, 'M', "Black", "Mean", 1 );
+
+INSERT INTO bears ( id, name, age, gender, color, temperament, alive)
+VALUES (7, "Wendy", 1, 'F', "Black", "Mean", 1 );
+
+INSERT INTO bears ( id, name, age, gender, color, temperament, alive)
+VALUES (8, NULL, 25, 'M', "Black", "Mean", 1 );
